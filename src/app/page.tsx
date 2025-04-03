b2aec7fdc1e6066b7007f52a06eafa8fd04af232
ImpulsVL <stiepochkinv@mail.ru>
2025-04-04 01:47:35 +0500

    changes for selector

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import MainPage from '@/app/main/MainPage'
 // import { LogoutBtn } from '@/features/auth/logout'
 // import { CheckUser } from '@/shared/api/user'
 
-export default async function Home() {
+export default function Home() {
   // const user = await CheckUser()
 
   // if (user?.id) {
